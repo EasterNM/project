082a796b46d83525251f0cdde638777df9c88aef
EasterNM <jojonithiwit@gmail.com>
2025-06-26 20:38:46 +0700

    pwa revise

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -1,67 +1,92 @@
-const CACHE_NAME = 'storems-v1';
+const CACHE_NAME = 'storems-v1.0.0';
 const urlsToCache = [
   '/',
-  '/static/css/',
-  '/static/js/',
-  '/static/images/',
-  // Add other static files you want to cache
+  '/accounts/login/',
+  '/static/css/tailwind.css',
+  '/static/js/serviceworker.js',
+  '/static/images/icons/icon-192x192.png',
+  '/static/images/icons/icon-512x512.png',
+  // เพิ่ม static files อื่นๆ ที่ต้องการ cache
 ];
 
+// Install Event
 self.addEventListener('install', function(event) {
+  console.log('Service Worker: Installing...');
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
+        console.log('Service Worker: Caching files');
         return cache.addAll(urlsToCache);
+      })
+      .then(function() {
+        console.log('Service Worker: Install complete');
+        return self.skipWaiting();
       })
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        return fetch(event.request).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-    );
-});
-
-// Clean up old caches
+// Activate Event
 self.addEventListener('activate', function(event) {
+  console.log('Service Worker: Activating...');
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.map(function(cacheName) {
           if (cacheName !== CACHE_NAME) {
+            console.log('Service Worker: Deleting old cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
       );
+    }).then(function() {
+      console.log('Service Worker: Activation complete');
+      return self.clients.claim();
     })
   );
+});
+
+// Fetch Event
+self.addEventListener('fetch', function(event) {
+  // เฉพาะ GET requests
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // ถ้ามีใน cache ให้ return
+        if (response) {
+          return response;
+        }
+
+        // ถ้าไม่มีใน cache ให้ fetch และ cache
+        return fetch(event.request).then(
+          function(response) {
+            // ตรวจสอบ response ว่าถูกต้อง
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone response สำหรับ cache
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                // Cache เฉพาะ static files
+                if (event.request.url.includes('/static/') || event.request.url === self.location.origin + '/') {
+                  cache.put(event.request, responseToCache);
+                }
+              });
+
+            return response;
+          }
+        );
+      }).catch(function() {
+        // ถ้า offline และไม่มีใน cache
+        if (event.request.destination === 'document') {
+          return caches.match('/');
+        }
+      })
+    );
 });
